f6aa698c222ee9708beaeb64566ec27a5633fdca
Edgar Zambrana <edgarjoel0920@gmail.com>
2024-03-02 14:14:37 -0500

    Create Recommendations component

---
+++ src/app/components/recommendations/recommendations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fin-recommendations',
+  templateUrl: './recommendations.component.html',
+  styleUrls: ['./recommendations.component.css']
+})
+export class RecommendationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
