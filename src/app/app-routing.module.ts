54dd3d41be81656157edcc040c99eae4a150aac0
Edgar Zambrana <edgarjoel0920@gmail.com>
2024-03-02 14:22:40 -0500

    Update routing paths. Update header to include new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginRegisterComponent } from './components/login-register/login-register.component';
+import { BrowseComponent } from './components/browse/browse.component';
+import { RecommendationsComponent } from './components/recommendations/recommendations.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginRegisterComponent },
+  { path: 'browse', component: BrowseComponent },
+  { path: 'recommendations', component: RecommendationsComponent },
+  { path: 'account', component: AccountsComponent },
   { path: '**', redirectTo: '' }
 ];
 
