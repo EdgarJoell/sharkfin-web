a2fb391c8b3c59fcedf3c14ee104c3f668edb2a9
Edgar Zambrana <edgarjoel0920@gmail.com>
2024-03-02 22:22:52 -0500

    Finish writing Home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalfuncsService } from 'src/app/service/globalfuncs.service';
 
 @Component({
   selector: 'fin-home',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public global: GlobalfuncsService) { }
 
   ngOnInit(): void {
   }
