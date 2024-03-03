6581188d0666e1e074bead9008189d25a79e8fc7
Edgar Zambrana <edgarjoel0920@gmail.com>
2024-03-02 22:19:54 -0500

    Add globalfuncs service as a provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
+import { GlobalfuncsService } from './service/globalfuncs.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { ComponentsModule } from './components/components.module';
     AppRoutingModule,
     ComponentsModule
   ],
-  providers: [],
+  providers: [GlobalfuncsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
